aa0af4cd9ad2a800bddeb42acc680310dd976a24
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-01 20:02:34 +0200

    with colors claswork

--- src/components/Instrument/Instrument.tsx
+++ src/components/Instrument/Instrument.tsx
@@ -6,6 +6,7 @@
 // Создайте три элемента с разными значениями пропсов
 // Дополнительно - подключите стили
 import React from 'react';
+import styles from './Instrument.module.css';
 
 interface Props {
   name: string;
@@ -16,8 +17,8 @@ interface Props {
 function Instrument(props:Props): JSX.Element {
   let { name, dateOfCreation, typeOfInstrument } = props;
   return (
-    <div>
-      <h2>Музыкальный инструмент</h2>
+    <div className={styles.mainData}>
+      <h2 className={styles.general}>Музыкальный инструмент</h2>
       <p>Название: {name}; Дата производства: {dateOfCreation}; Тип инструмента: {typeOfInstrument}; </p>
     </div>
   );
