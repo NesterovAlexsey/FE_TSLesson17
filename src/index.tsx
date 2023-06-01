72f2f960a37e0b870db26ad3c3d301bf6fb13278
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-01 19:24:52 +0200

    colors of car

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,19 @@ import './index.css';
 import App from './components/App/App';
 import Counter from './components/Counter/Counter';
 import Sandwich from './components/homework17/BurgerButton';
+import Car from './components/Car/Car';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Car color="black" brand="BMW" />
+    <Car color="yellow" brand="Ferrari" />
+    <Car color="yellow" brand="Submmarine" />
+    <Car color="grey" brand="Kia" />
+    <Car color="red" brand="Lamborghini" />
+    <Car color="violet" brand="Lada" />
     <Sandwich />
     <Counter />
     <App />
