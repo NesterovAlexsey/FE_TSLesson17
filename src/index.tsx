83d925fdce9436df3678f4b5c9e7aae2a8400fa3
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-01 17:50:41 +0200

    refactor index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App'
+import App from './components/App/App';
 import Counter from './components/Counter/Counter';
 import Sandwich from './components/homework17/BurgerButton';
 
@@ -10,10 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Sandwich />    
+    <Sandwich />
     <Counter />
     <App />
   </React.StrictMode>
 );
-
-
