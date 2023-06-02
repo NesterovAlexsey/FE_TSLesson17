279cf1043ec2caf98ef5548827b34529e389c673
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-02 07:37:05 +0200

    connected FlowerCard with Parent and show draft  at screen

--- src/components/Parent/Parent.tsx
+++ src/components/Parent/Parent.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PirateCard from '../PirateCard/PirateCard';
 import Pirate from '../../types/Pirate';
-// import Flower from '../../types/Flower';
-// import FlowerCard from '../FlowerCard/FlowerCard';
+import Flower from '../../types/Flower';
+import FlowerCard from '../FlowerCard/FlowerCard';
 
 function Parent(): JSX.Element {
   const john: Pirate = {
@@ -12,8 +12,16 @@ function Parent(): JSX.Element {
     numberOfLegs: 2
   };
 
+  const kaktus: Flower = {
+    color: "green",
+    title: "Sweety",
+    kind: "Cactaceae",
+    price: 100,
+  }
+
   return (
     <div>
+      <FlowerCard flower={kaktus}/>
       <h2>Родительский компонент</h2>
       <PirateCard pirate={john} />
     </div>
